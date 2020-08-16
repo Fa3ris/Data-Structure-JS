dd81968e9e83bfad5870f722d0ada95a315fb3a6
Fa3ris <Fa3ris@users.noreply.github.com>
2020-08-16 17:24:56 +0200

    add bst features

--- binary-search-tree.mjs
+++ binary-search-tree.mjs
@@ -45,7 +45,7 @@ export class BST {
             }
         }
 
-        function addRight(node){
+        function addRight(node) {
             if (node.right === null) {
                 node.right = new Node(data);
             } else {
@@ -54,6 +54,204 @@ export class BST {
         }
     }
 
+    getMin() {
+        let current = this.root;
+        while (current.left !== null) {
+            current = current.left;
+        }
+        return current.data;
+    }
+
+    getMax() {
+        let current = this.root;
+        while (current.right !== null) {
+            current = current.right;
+        }
+        return current.data;
+    }
+
+    has(data) {
+        let current = this.root;
+        while (current) {
+            if (data === current.data) { return true; }
+
+            current = (data < current.data) ? current.left : current.right;
+        }
+
+        return false;
+    }
+
+    remove(data) {
+
+        this.root = removeNode(this.root, data);
+
+        function removeNode(node, data) {
+
+            // data to remove not found
+            if (node === null) {
+                return null;
+            }
+
+            // perform removal
+            if (data === node.data) {
+
+                return getNewNode(node);
+
+                // remove in left subtree
+            } else if (data < node.data) {
+                node.left = removeNode(node.left, data);
+                return node;
+
+                // remove in right subtree
+            } else {
+                node.right = removeNode(node.right, data);
+                return node;
+            }
+
+            /**
+             * return the reference to the new node after removal
+             * @param node actual node
+             */
+            function getNewNode(node) {
+                // no child -> remove
+                if (node.left === null && node.right === null) {
+                    return null;
+                }
+
+                // only one right child -> return right child
+                else if (node.left === null) {
+                    return node.right;
+                }
+
+                // only one left child -> return left child
+                else if (node.right === null) {
+                    return node.left;
+                }
+
+                // both children
+                else {
+
+                    // find min in right subtree and replace node data
+                    const minNode = getMinNode(node.right);
+                    node.data = minNode.data;
+
+                    // remove min from right subtree
+                    node.right = removeNode(node.right, minNode.data);
+                    return node;
+                }
+            }
+
+            /**
+             * return node with minimum value
+             * @param current 
+             */
+            function getMinNode(current) {
+                while (current.left !== null) {
+                    current = current.left;
+                }
+                return current;
+            }
+
+        }
+    }
+
+    // BREADTH FIRST SEARCH
+
+    levelOrder() {
+
+        if (this.root === null) {
+            return null;
+        }
+
+        const data = new Array();
+        const queue = Array();
+
+        queue.push(this.root);
+        while (queue.length > 0) {
+            // dequeue
+            let node = queue.shift();
+
+            data.push(node.data);
+            if (node.left !== null) {
+                // enqueue
+                queue.push(node.left);
+            }
+            if (node.right !== null) {
+                // enqueue
+                queue.push(node.right);
+            }
+        }
+        return data;
+
+    }
+
+    // DEPTH FIRST SEARCH
+
+    // N=node
+    // L=left-subtree
+    // R=right-subtree
+
+    /**
+     * inOrder (L->N->R)
+     * return values from left-most to right-most node
+     */
+    inOrder() {
+        if (this.root) {
+            return null;
+        }
+
+        const data = new Array();
+        traverseInOrder(this.root);
+        return data;
+
+        function traverseInOrder(node) {
+            // short-circuit
+            node.left && traverseInOrder(node.left);
+            data.push(node.data);
+            node.right && traverseInOrder(node.right);
+        }
+    }
 
 
+    /**
+     * preOrder (N->L->R)
+     * return values starting from roots
+     */
+    preOrder() {
+        if (this.root === null) {
+            return null;
+        }
+
+        const data = new Array();
+        traverseInPostOrder(this.root);
+        return data;
+        
+        function traverseInPostOrder(node) {
+            data.push(node.data);
+            // short-circuit
+            node.left && traverseInPostOrder(node.left);
+            node.right && traverseInPostOrder(node.right);
+        }
+    }
+
+    /**
+     * postOrder (L->R->N)
+     * return values starting from leaves
+     */
+    postOrder() {
+        if (this.root === null) {
+            return null;
+        }
+
+        const data = new Array();
+        traverseInPostOrder(this.root);
+        return data;
+        
+        function traverseInPostOrder(node) {
+            // short-circuit
+            node.left && traverseInPostOrder(node.left);
+            node.right && traverseInPostOrder(node.right);
+            data.push(node.data);
+        }
+    }
 }
--- main-bst.mjs
+++ main-bst.mjs
@@ -9,3 +9,18 @@ bst.add(6);
 bst.add(4);
 bst.add(11);
 bst.add(1);
+
+console.log(`tree min = ${bst.getMin()}`);
+console.log(`tree max = ${bst.getMax()}`);
+
+console.log(`tree has 4 : ${bst.has(4)}`);
+console.log(`tree has 26 : ${bst.has(26)}`);
+console.log(`tree has 6 : ${bst.has(6)}`);
+
+bst.remove(6);
+console.log(`tree has 6 : ${bst.has(6)}`);
+
+console.log(`inOrder: ${bst.inOrder()}`);
+console.log(`preOrder: ${bst.preOrder()}`);
+console.log(`postOrder: ${bst.postOrder()}`);
+console.log(`levelOrder: ${bst.levelOrder()}`);
