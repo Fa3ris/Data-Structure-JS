b74100581abf800e85995a0f934d46a61f34161b
Fa3ris <Fa3ris@users.noreply.github.com>
2020-08-30 22:54:53 +0200

    add dijkstra implementation

--- graph.mjs
+++ graph.mjs
@@ -30,7 +30,7 @@ export class Graph {
             throw new Error(`${end} not present`);
         }
 
-        this.nodes.get(start).connectNode(this.nodes.get(end), dist);
+        this.nodes.get(start).connectNode(end, dist);
     }
 
     /**
@@ -50,8 +50,8 @@ export class Graph {
             throw new Error(`${end} not present`);
         }
 
-        this.nodes.get(start).connectNode(this.nodes.get(end), dist);
-        this.nodes.get(end).connectNode(this.nodes.get(start), dist);
+        this.nodes.get(start).connectNode(end, dist);
+        this.nodes.get(end).connectNode(start, dist);
     }
 
 
@@ -61,8 +61,8 @@ export class Graph {
     print() {
         for (const [name, node] of this.nodes.entries()) {
             console.group(`${name} has following neighbors`)
-            for (const [edge, dist] of node.neighbors.entries()) {
-                console.log(`${edge.name} : ${dist}`);
+            for (const [name, dist] of node.neighbors.entries()) {
+                console.log(`${name} : ${dist}`);
             }
             console.groupEnd();
         }
@@ -73,13 +73,90 @@ export class Graph {
      * return object containing
      * list of shortest distance from root to each node
      * list of shortest path from root to each node
-     * @param root 
+     * @param root name of root node
      */
     dijkstra(root) {
-        const dist = [], paths = [];
 
-        // TODO
+        root = root.toLowerCase();
+
+        if (!this.nodes.has(root)) {
+            throw new Error(`${root} not present`);
+        }
+
+        const dist = new Map(), // dist of each node to root { [node.name] : dist }
+            previous = new Map(), // parent node of each node to reach root { [node.name] : parent.name | null }
+            unvisited = new Set(); // unvisited/unprocessed nodes { node.name }
+
+        this.nodes.forEach((value, key) => {
+            dist.set(key, Infinity);
+            previous.set(key, null);
+            unvisited.add(key);
+        });
+
+        dist.set(root, 0);
+
+        let current = root;
+
+        while (unvisited.size > 0) {
+
+            let currentNode = this.nodes.get(current);
+
+            for (const [neighbor, neighborDist] of currentNode.neighbors.entries()) {
+                // console.log(`${neighbor.name} : ${dist}`);
+
+                if (unvisited.has(neighbor)) {
+                    // dist root to current + dist current to neighbor
+                    const tempDist = dist.get(current) + neighborDist;
+
+                    if (tempDist < dist.get(neighbor)) {
+                        dist.set(neighbor, tempDist);
+                        previous.set(neighbor, current);
+                    }
+                }
+            }
+
+            unvisited.delete(current);
+
+            // find shortest path node among unvisited
+
+            // inefficient
+            const list = new Array();
+            
+            unvisited.forEach(value => {
+                list.push(value);
+            })
+
+            current = list.shift();
+
+            list.forEach(value => {
+                if (dist.get(value) < dist.get(current)) {
+                    current = value;
+                }
+            })
+
+            if (dist.get(current) === Infinity) {
+                break;
+            }
+
+        }
+
+        const paths = new Map();
+
+        this.nodes.forEach((value, key) => {
         
+            const path = new Array();
+
+            path.push(key);
+            let previousName = previous.get(key);
+            while (previousName != null) {
+                path.push(previousName);
+                previousName = previous.get(previousName);
+            }
+            path.reverse();
+            paths.set(key, path);
+
+        });
+
         return {dist, paths};
 
     }
@@ -92,9 +169,13 @@ class Node {
         this.neighbors = new Map();
     }
 
-    connectNode(node, dist) {
-        if (node.name !== this.name) {
-            this.neighbors.set(node, dist);
+    connectNode(name, dist) {
+        if (name !== this.name) {
+            this.neighbors.set(name, dist);
         }
+    }
+
+    dist(name) {
+        return this.neighbors.has(name) ? this.neighbors.get(name) : Infinity;
     }
 }
--- main-dijkstra.mjs
+++ main-dijkstra.mjs
@@ -35,4 +35,48 @@ try {
 }
 
 
-graph.print();
+// graph.print();
+
+const g2 = new Graph();
+g2.addNode('a');
+g2.addNode('b');
+g2.addNode('c');
+g2.addNode('d');
+g2.addNode('e');
+g2.addNode('f');
+g2.addNode('impossible');
+
+
+g2.connectNodes('a', 'b', 4);
+g2.connectNodes('a', 'c', 4);
+
+g2.connectNodes('b', 'c', 2);
+
+g2.connectNodes('c', 'd', 3);
+g2.connectNodes('c', 'e', 1);
+g2.connectNodes('c', 'f', 6);
+
+g2.connectNodes('d', 'f', 2);
+g2.connectNodes('e', 'f', 3);
+
+// one way connection
+g2.connectNode('impossible', 'f', 100);
+
+// g2.print();
+
+const resA = g2.dijkstra('a');
+
+console.log(resA.dist);
+console.log(resA.paths);
+
+
+const resB = g2.dijkstra('b');
+
+console.log(resB.dist);
+console.log(resB.paths);
+
+
+const resImpossible = g2.dijkstra('impossible');
+
+console.log(resImpossible.dist);
+console.log(resImpossible.paths);
